714ee87fca2f2702bf5f1e3d38d833a2b75cbdf1
jt sellers <jonnie.sellers@gmail.com>
2020-10-23 14:21:45 -0400

    first commit

---
+++ Sociality.Data/TheUser.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Sociality.Data
+{
+    public class TheUser
+    {
+        [Key]
+        public int UserKey { get; set; }
+
+        [Required]
+        public Guid UserId { get; set; }
+
+        [Required]
+        public string name { get; set; }
+
+        [Required]
+        public string Email { get; set; }
+    }
+}
